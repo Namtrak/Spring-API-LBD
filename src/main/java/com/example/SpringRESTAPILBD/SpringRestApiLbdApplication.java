2a0b483da8edadbd3e87555d5a8b0642492ca8fe
Namtrak <mateosxxx@gmail.com>
2022-07-18 09:55:33 +0200

    Initialized project with Spring Initializr

---
+++ src/main/java/com/example/SpringRESTAPILBD/SpringRestApiLbdApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringRESTAPILBD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApiLbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestApiLbdApplication.class, args);
+	}
+
+}
